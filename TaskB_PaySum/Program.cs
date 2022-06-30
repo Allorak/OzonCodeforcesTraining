c4d48d3959fc383672b65acfabffe37094f253c4
Allorak <allorakspam@gmail.com>
2022-06-30 16:46:35 +0300

    Task B complete

---
+++ TaskB_PaySum/Program.cs
@@ -0,0 +1,22 @@
+﻿var setAmount = int.Parse(Console.ReadLine());
+for (var i = 0; i < setAmount; i++)
+{
+    var itemsAmount = int.Parse(Console.ReadLine());
+    var prices = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    var shoppingList = new Dictionary<int, int>();
+    foreach (var price in prices)
+    {
+        if (!shoppingList.ContainsKey(price))
+            shoppingList[price] = 1;
+        else
+            shoppingList[price] += 1;
+    }
+    var totalPrice = 0;
+    foreach (var (price, amount) in shoppingList)
+    {
+        var amountWithSale = amount / 3 * 2 + amount % 3;
+        totalPrice += amountWithSale * price;
+    }
+    
+    Console.WriteLine(totalPrice);
+}
