4dffeea0b70dded68e2bc7f6a42ecbcca1e2c647
Allorak <allorakspam@gmail.com>
2022-06-30 16:46:24 +0300

    Task A complete

---
+++ TaskA_Summator/Program.cs
@@ -0,0 +1,6 @@
+﻿int pairAmount = int.Parse(Console.ReadLine());
+for (int i = 0; i < pairAmount; i++)
+{
+    int[] pair = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    Console.WriteLine(pair[0] + pair[1]);
+}
